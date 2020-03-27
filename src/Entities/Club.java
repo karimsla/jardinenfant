d1fa14b44cdfe46c2e44f710b276b1962807982a
dorra <dorra.kerro@gmail.com>
2020-03-27 20:52:47 +0100

    getters nd constructors done

--- src/Entities/Club.java
+++ src/Entities/Club.java
@@ -5,6 +5,9 @@
  */
 package Entities;
 
+import java.util.List;
+import java.util.Objects;
+
 /**
  *
  * @author karim
@@ -22,5 +25,87 @@ public class Club {
 
     
     private String photo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public Club() {
+    }
+
+    public Club(int id, String name, String description, String photo) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.photo = photo;
+    }
+
+    @Override
+    public String toString() {
+        return "Club{" + "id=" + id + ", name=" + name + ", description=" + description + ", photo=" + photo + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Club other = (Club) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.photo, other.photo)) {
+            return false;
+        }
+        return true;
+    }
+    
+   
     
 }
