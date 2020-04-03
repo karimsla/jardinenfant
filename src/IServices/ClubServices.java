1d267458b9a28a07d08c8d152e3c99cec3650cd0
dorra <dorra.kerro@gmail.com>
2020-04-03 02:01:25 +0100

    services

--- src/IServices/ClubServices.java
+++ src/IServices/ClubServices.java
@@ -5,11 +5,14 @@
  */
 package IServices;
 
+import Entities.Activite;
 import Entities.Club;
 import Utils.ConnexionBD;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -40,6 +43,58 @@ public class ClubServices {
           
 
       
+  }
+     
+     
+      public static int modifier(String nom, String desc, Integer id){
+      
+      int ac = 0;
+      
+        try{
+             
+             Connection con = ConnexionBD.getInstance().getCnx();
+            String res="Update Club SET Name=? ,Description=? WHERE id="+id;
+            PreparedStatement pre = con.prepareStatement(res); 
+            pre.setString(1,nom);
+            pre.setString(2,desc);
+           
+
+            ac= pre.executeUpdate();
+            
+            
+        }catch(SQLException ex){
+            System.out.println(ex);
+        }
+        catch(Exception es)
+        {
+            System.err.println(es);
+        }
+      
+      return ac;
+  }
+      
+      
+        public static int Delete(Integer id ){
+        int d = 0 ;
+        try {
+          
+            
+            Connection con = ConnexionBD.getInstance().getCnx();
+            
+            String res="DELETE FROM Club Where id="+id ;
+            
+            PreparedStatement stat = con.prepareStatement(res);
+         
+           d =  stat.executeUpdate(res);
+            
+            
+            
+           
+        } catch (SQLException ex) {
+            Logger.getLogger(ActiviteServices.class.getName()).log(Level.SEVERE, null, ex);
+        }
+         
+            return d ;
   }
     
 }
