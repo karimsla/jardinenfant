198a87620c0adc48ca570071e706c62125a45b0e
dorra <dorra.kerro@gmail.com>
2020-04-14 13:20:46 +0100

    before ekher mara

--- src/Views/ConsulterActiviteController.java
+++ src/Views/ConsulterActiviteController.java
@@ -24,12 +24,16 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+
+
 
 /**
  * FXML Controller class
@@ -69,8 +73,14 @@ public class ConsulterActiviteController implements Initializable {
         HashMap<String, Integer> map;
     @FXML
     private Button Club_btn;
+    @FXML
+    private Button qr;
+    @FXML
+    private Button weather_btn;
+    @FXML
+    private TextField search_txt;
     
-     @FXML
+    
     private void Afficher(){
         
 
@@ -109,7 +119,7 @@ public class ConsulterActiviteController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        
+        Afficher();
          map = new HashMap<>();  
         LoadData();
           
@@ -195,9 +205,69 @@ public class ConsulterActiviteController implements Initializable {
     @FXML
     private void TakeMeToClub(ActionEvent event) throws IOException {
         
-          if(event.getSource() == Club_btn){
+          {
             AnchorPane pane = FXMLLoader.load(getClass().getResource("Club/ConsulterClub.fxml"));
             root.getChildren().setAll(pane);
     }
     }
+
+    @FXML
+    private void qrCode(ActionEvent event) throws SQLException {
+        
+          QrCodeController q=new QrCodeController();
+       
+       
+         if (tableAct.getSelectionModel().getSelectedItem() == null) {
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("WARNING");
+                alert.setContentText("Aucun element n'est selectionné");
+                alert.showAndWait();
+            }
+            else{
+        q.ini(tableAct.getSelectionModel().getSelectedItem());
+    }
+    }
+
+    @FXML
+    private void TakeMeToWeather(ActionEvent event) throws IOException {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("Weather.fxml"));
+            root.getChildren().setAll(pane);
+        
+    }
+
+    @FXML
+    private void Search(ActionEvent event) {
+         data.clear();
+         String nom = search_txt.getText();
+           
+        try{
+            Connection con = (Connection) ConnexionBD.getInstance().getCnx();
+            String res="Select * from Activite where typeact Like '%"+nom+"%' "   ;
+          
+            Statement statement = con.createStatement();
+          
+            ResultSet rs =  statement.executeQuery(res);
+            while(rs.next()){
+                 Activite p = new Activite();
+                 p.setTypeact(rs.getString("typeact"));
+                 p.setDetailles(rs.getString("detailles"));
+                 p.setDate(rs.getString("date"));
+                
+                data.add(p);
+            }
+        } catch (SQLException ex) {
+             Logger.getLogger(ConsulterActiviteController.class.getName()).log(Level.SEVERE, null, ex);
+         }
+        
+        activite.setCellValueFactory(new PropertyValueFactory<Activite,String>("typeact"));
+        description.setCellValueFactory(new PropertyValueFactory<Activite,String>("detailles"));
+        date.setCellValueFactory(new PropertyValueFactory<Activite,Date>("date"));
+        tableAct.setItems(data);
+        
+        
+        search_txt.clear();
+         
+    }
+
+
 }
